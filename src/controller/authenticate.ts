b09af2cea6b8c872c5f4c30f8522d59ddee400f6
AlexDeKatz <alecdekatz@gmail.com>
2020-09-04 23:15:57 +0530

    Configurd Heroku redis to cache the refresh token. (Pending test)

--- src/controller/authenticate.ts
+++ src/controller/authenticate.ts
@@ -1,12 +1,62 @@
 /* eslint-disable no-console */
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import { redisClient } from '../server';
 
 dotenv.config();
 export const authController = express.Router();
 
-authController.post('/login', (req: Request, res: Response) => {
-  const accessToken: string = jwt.sign(req.body, process.env.ACCESS_TOKEN_SECRET as string);
-  res.json({ accessToken });
+authController.post('/login', (req: Request, res: Response, next: NextFunction) => {
+  const accessToken: string = generateAccessToken(req.body);
+  const refreshToken: string = jwt.sign(req.body, process.env.REFRESH_TOKEN_SECRET as string);
+  // refreshTokenDB.push(refreshToken);
+  redisClient.sadd('refreshTokenDB', refreshToken, (err: any, data: any) => {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Data: ', data);
+      res.json({ accessToken, refreshToken });
+    }
+  });
 });
+
+authController.post('/refresh', (req: Request, res: Response, next: NextFunction) => {
+  const refreshToken = req.body.token;
+  if (refreshToken === null) {
+    res.sendStatus(401);
+  } else {
+    redisClient.sismember('refreshTokenDB', refreshToken, (err: any, data: any) => {
+      if (err) {
+        next(err);
+      } else {
+        console.log('Data: ', data);
+        // res.sendStatus(403);
+        jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET as string, (err: any, user: any) => {
+          if (err) {
+            res.sendStatus(403);
+          } else {
+            const { name, email } = user;
+            const accessToken: string = generateAccessToken({ name, email });
+            res.json({ accessToken });
+          }
+        });
+      }
+    });
+  }
+});
+
+authController.delete('/logout', (req: Request, res: Response, next: NextFunction) => {
+  redisClient.srem('refreshTokenDB', req.body.token, (err: any, data: any) => {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Data: ', data);
+      res.sendStatus(204);
+    }
+  });
+});
+
+function generateAccessToken (payload: any): string {
+  return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET as string, { expiresIn: '10s' });
+}
--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,18 @@ import dotenv from 'dotenv';
 import io from 'socket.io';
 import { createServer } from 'http';
 import { ViewPortServer } from './app';
+import redis from 'redis';
+import { URL } from 'url';
 
 dotenv.config();
 const port = process.env.SERVER_PORT;
 const app = new ViewPortServer().app;
 const server = createServer(app);
+const redisURL: URL = new URL(process.env.REDISCLOUD_URL as string);
 export const websocket: any = io(server);
+export const redisClient = redis.createClient(parseInt(redisURL.port, 10), redisURL.hostname, { no_ready_check: true });
+
+redisClient.auth(redisURL.password);
 
 websocket.on('connect', (socket: any) => {
   console.log('Connected client on port %s', port);
@@ -21,6 +27,14 @@ websocket.on('connect', (socket: any) => {
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
+});
+
+redisClient.on('connect', () => {
+  console.log('Reddis connected');
+});
+
+redisClient.on('error', () => {
+  console.log('Connection to redis failed');
 });
 
 server.listen(port, () => {
